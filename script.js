272e7eef4895a391d15d63cbc9d333dfc7f260de
Suci Rohmawati <sici.rohma352@gmail.com>
2025-08-30 15:15:16 +0700

    menentukan status

--- script.js
+++ script.js
@@ -8,4 +8,20 @@ btnCalculate.addEventListener ('click', (event) =>{
     let bodyHeight = parseFloat(document.getElementById('height').value) /100
     let bmi = bodyWeight / (bodyHeight**2)
     console.log(bmi)
-})
+
+    let status =''
+    if (bmi < 18.5){
+        status = 'underweight'
+    } else if ( bmi < 25){
+        status = 'normal'
+    } else if ( bmi < 30){
+        status = 'overweight'
+    } else (
+        status = 'obesity'
+    )
+    console.log(status)
+
+   
+}
+
+)
